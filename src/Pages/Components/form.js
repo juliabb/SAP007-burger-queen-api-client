3ab9a7e29213f710c799ae1509d5803e038eb776
Tatiane Mendonça <tatimendonz@gmail.com>
2022-05-14 02:50:51 -0300

    Ajuste na responsividade

---
+++ src/Pages/Components/form.js
@@ -0,0 +1,8 @@
+// export function Validate({login, senha}){
+//     return(
+//         <div>
+//             <p>{login}</p>
+//             <p>{senha}</p>
+//         </div>
+//     )
+// }
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,15 +2,20 @@ import './home.css'
 import logo from "../Style/Images/logo-.png"
 
 export function Home() {
+
+  function buttonClicked() {
+    alert("Botão clicado!")
+  }
+
   return (
     <div className='main'>
-    <div className='container'>
-      <img src={logo} alt='Logo Mr Simpsons'/>
-     <h1>Login</h1>
-     <input type="text" placeholder="Login"/>
-     <input type="text" placeholder="Senha"/>
-     <button type="button">Entrar</button>
+      <div className='container'>
+        <img src={logo} alt='Logo Mr Simpsons' className='logo-hommer' />
+        <h1>Login</h1> 
+        <input type="text" placeholder="Login" />
+        <input type="text" placeholder="Senha"/>
+       <button type="button" onClick={buttonClicked}>Entrar</button>
     </div>
     </div>
-  );
-}
+  ); 
+};
