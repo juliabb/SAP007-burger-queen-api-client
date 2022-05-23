b0955f6ab904d3315b4d5f17d732d0ec5f0a2c7f
Julia Benedicto <68789655+juliabb@users.noreply.github.com>
2022-05-23 20:47:38 -0300

    Revert "Feat login"

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div>
+     <h1>Oi</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/components/Header/index.jsx
+++
@@ -1,8 +0,0 @@
-import logo from "../../Pages/Style/Images/logo-name.png"
-
-function Header() {
-    return (
-        <img src={logo} alt='Mr Simpsons' className='mr-simpsons' />
-    )
-}
-export default Header
