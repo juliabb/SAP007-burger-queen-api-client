c1da66f3aac664ce7ba0338cc254006168f75327
Tatiane Mendonça <97405991+Tati-Mendonca@users.noreply.github.com>
2022-05-25 16:42:41 -0300

    Resolvendo bug
    Co-authored-by: juliabb <juliabenedictohfvcjd@gmail.com>
    Co-authored-by: Tati-Mendonca <tatimendozn@gmail.com>

---
+++ src/Pages/Error/error.js
@@ -0,0 +1,16 @@
+function Errors(response){
+    switch (response.status){
+    case 400:
+    return "Dados inválidos ou ausentes";
+    case 401:
+    return "Usuário não encontrado";
+    case 403:
+    return "Acesso negado, tente outro e-mail";
+    case 404:
+    return "Usuário não encontrado";
+    default:
+    return "Ocorreu algum erro";
+    }
+    }
+    
+    export default Errors
