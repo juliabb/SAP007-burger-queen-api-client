cb8e84f08a8d1462c135be7d598a0fcbe46287fd
Julia Benedicto <juliabenedictohfvcjd@gmail.com>
2022-05-27 14:43:29 -0300

    ok

--- src/Pages/Kitchen/index.js
+++ src/Pages/Kitchen/index.js
@@ -1,8 +1,6 @@
-/* import ButtonLogout from "../../components/Button/Button-logout" */
 function Kitchen() {
     return (<div>
         <h1>Kitchen</h1>
-        {/* <ButtonLogout /> */}
     </div>
     )
 }
