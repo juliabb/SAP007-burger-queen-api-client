bb845a08b948afc765cb44ee4460155fe1b66211
Tatiane Mendonça <97405991+Tati-Mendonca@users.noreply.github.com>
2022-05-27 16:24:47 -0300

    Ajuste no Regex
    Co-authored-by: juliabb <juliabenedictohfvcjd@gmail.com>
    Co-authored-by: Tati-Mendonca <tatimendozn@gmail.com>

--- src/Pages/Authentication/index.js
+++ src/Pages/Authentication/index.js
@@ -7,4 +7,3 @@ export function validatePassword(password) {
    const senhaRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/
    return senhaRegex.test(String(password))
  }
-
